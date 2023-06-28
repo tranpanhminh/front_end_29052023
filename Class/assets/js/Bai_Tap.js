40b3178a87b475422970787eff7d25fb67ed8545
tranpanhminh <tranphuocanhminh73@gmail.com>
2023-06-28 22:49:10 +0700

    Update Session 19

--- Class/assets/js/Bai_Tap.js
+++ Class/assets/js/Bai_Tap.js
@@ -119,3 +119,179 @@ function bai_02() {
   dog.setSpeed(50);
   console.log("Tốc độ của chó mới: ", dog.getSpeed());
 }
+
+// <-------------------------------------> //
+// Script bài 4
+let courseArray = [];
+
+function bai_04() {
+  let inputValue = "";
+  while (inputValue !== "E") {
+    inputValue = prompt(
+      "Nhập vào 1 trong 5 chữ C / R / U / D / E: "
+    ).toUpperCase();
+    if (inputValue === "C") {
+      pressC();
+      render();
+    } else if (inputValue === "R") {
+      pressR();
+    } else if (inputValue === "U") {
+      pressU();
+    } else if (inputValue === "D") {
+      pressD();
+    }
+  }
+  alert("Cảm ơn bạn đã đến với Rikkei Academy");
+}
+
+function pressC() {
+  let inputCourse = prompt("Nhập khóa học: ");
+  let inputStatus = prompt("Tình trạng khóa học: ");
+
+  let course = {
+    name: inputCourse,
+    complete: inputStatus,
+  };
+
+  courseArray.push(course);
+}
+
+function render() {
+  let listCourse = document.querySelector(".list-course");
+  let courseContent = "";
+  for (let i = 1; i <= courseArray.length; i++) {
+    courseContent += `<p class="course-item">${i}. ${
+      courseArray[i - 1].name
+    }</p>
+    <p class="course-status">Complete: ${courseArray[i - 1].complete}</p>`;
+  }
+
+  listCourse.innerHTML = courseContent;
+}
+
+function pressR() {
+  const course_01 = new Course(1, "HTML", "false");
+  const course_02 = new Course(2, "CSS", "false");
+  const course_03 = new Course(3, "Basic of Javascript", "false");
+  const course_04 = new Course(4, "Node package Manager (npm)", "false");
+  const course_05 = new Course(5, "Git", "false");
+  let listCourse = document.querySelector(".list-course");
+  let courseContent = "";
+  courseArray.push(course_01, course_02, course_03, course_04, course_05);
+  for (i = 0; i < courseArray.length; i++) {
+    courseContent += `<p class="course-item">${courseArray[i].id}. ${courseArray[i].name}</p>
+    <p class="course-status">Complete: ${courseArray[i].complete}</p>`;
+  }
+  listCourse.innerHTML = courseContent;
+  render(courseArray);
+}
+
+function Course(id, name, complete) {
+  this.id = id;
+  this.name = name;
+  this.complete = complete;
+}
+
+function pressU() {
+  let inputIndex = prompt("Nhập vào vị trí khóa học bạn muốn edit");
+  let newCourseName = prompt("Nhập vào tên Course mới");
+  let newCourseComplete = prompt("Nhập vào tình trạng của Course mới");
+
+  let newCourse = {
+    name: newCourseName,
+    complete: newCourseComplete,
+  };
+
+  inputIndex = parseInt(inputIndex) - 1;
+  courseArray.splice(inputIndex, 1, newCourse);
+  render(courseArray);
+}
+
+function pressD() {
+  let inputIndex = prompt("Nhập vào vị trí khóa học bạn muốn xóa");
+  inputIndex = parseInt(inputIndex) - 1;
+  courseArray.splice(inputIndex, 1);
+  render(courseArray);
+}
+
+// <-------------------------------------> //
+// Script bài 5
+let stores = [
+  { id: 1, name: "Milk", count: 100 },
+  { id: 2, name: "Yakult", count: 100 },
+  { id: 3, name: "Butter", count: 100 },
+];
+let carts = [];
+
+function bai_05() {
+  let inputValue = "";
+  while (inputValue !== "E") {
+    inputValue = prompt(
+      "Nhập vào 1 trong 5 chữ C / R / U / D / E: "
+    ).toUpperCase();
+    if (inputValue === "C") {
+      pressC(stores);
+    } else if (inputValue === "R") {
+      pressR();
+    } else if (inputValue === "U") {
+      pressU();
+    } else if (inputValue === "D") {
+      pressD();
+    }
+  }
+  alert("Cảm ơn bạn đã đến với Rikkei Stores");
+}
+
+let buyProduct = [];
+
+function pressC() {
+  let inputProduct = prompt("Nhập tên sản phẩm muốn mua:");
+  let inputCart = Number(prompt("Nhập số lượng sản phẩm bạn muốn mua: "));
+
+  for (let i = 0; i < stores.length; i++) {
+    if (stores[i].name.toLowerCase() == inputProduct.toLowerCase()) {
+      stores[i].count -= inputCart;
+    }
+  }
+
+  let product = {
+    name: inputProduct,
+    count: inputCart,
+  };
+
+  buyProduct.push(product);
+
+  let sumCart = 0;
+  for (let i = 0; i < buyProduct.length; i++) {
+    sumCart += buyProduct[i].count;
+  }
+
+  let listBuy = document.querySelector(".buy-product");
+  let listBuyContent = "";
+  listBuyContent += ` <p class="cart">Cart: ${sumCart}</p>`;
+  for (let i = 0; i < buyProduct.length; i++) {
+    listBuyContent += `<p class="product-name">${i + 1}. ${
+      buyProduct[i].name
+    }</p>
+    <p class="product-count">Count: ${buyProduct[i].count}</p>`;
+  }
+
+  listBuy.innerHTML = listBuyContent;
+  render(stores);
+}
+
+function pressR() {
+  render(stores);
+}
+
+function render(stores) {
+  let listStore = document.querySelector(".list-store");
+  let listStoreContent = "";
+  listStoreContent += ` <p class="cart">Cart: 0</p>`;
+  for (let i = 0; i < stores.length; i++) {
+    listStoreContent += `<p class="store-name">${i + 1}. ${stores[i].name}</p>
+    <p class="count">Count: ${stores[i].count}</p>`;
+  }
+
+  listStore.innerHTML = listStoreContent;
+}
